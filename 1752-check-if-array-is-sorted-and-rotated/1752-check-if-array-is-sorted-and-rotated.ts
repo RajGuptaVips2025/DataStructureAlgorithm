506c3d3a23001090dc4399828542b5f7721833a1
Raj Gupta <168336609+RajGuptaVips2025@users.noreply.github.com>
2026-07-04 03:24:39 +0530

    Time: 0 ms (100%), Space: 54.9 MB (71.04%) - LeetHub

---
+++ 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.ts
@@ -0,0 +1,9 @@
+function check(nums: number[]): boolean {
+    let count = 0;
+
+    for(let i = 0; i < nums.length; i++){
+        if(nums[i] > nums[(i+1) % nums.length]) count++;
+        if(count > 1) return false;
+    }
+    return true;
+};
