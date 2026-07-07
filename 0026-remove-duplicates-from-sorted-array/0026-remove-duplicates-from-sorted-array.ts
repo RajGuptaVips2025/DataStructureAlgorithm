057f60b9d4b96e2388434232a9e94b68e2a67d90
Raj Gupta <168336609+RajGuptaVips2025@users.noreply.github.com>
2026-07-07 23:16:17 +0530

    Time: 0 ms (100%), Space: 58.6 MB (58.24%) - LeetHub

---
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.ts
@@ -0,0 +1,11 @@
+function removeDuplicates(nums: number[]): number {
+    let i = 0;
+
+    for(let j = 1; j<nums.length; j++){
+        if(nums[j] != nums[i]){
+            nums[i+1] = nums[j]
+            i++
+        }
+    }
+    return i+1;
+};
