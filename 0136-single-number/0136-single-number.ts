e95906989eaa3f6d788e68490a268ea7517715f9
Raj Gupta <168336609+RajGuptaVips2025@users.noreply.github.com>
2026-07-13 17:37:22 +0530

    Time: 362 ms (5.09%), Space: 56.4 MB (83.31%) - LeetHub

---
+++ 0136-single-number/0136-single-number.ts
@@ -0,0 +1,15 @@
+// brute for approach
+function singleNumber(nums: number[]): number {
+    const n = nums.length;
+    for (let i = 0; i < n; i++) {
+        let num = nums[i];
+        let count = 0;
+        for (let j = 0; j < n; j++) {
+            if (nums[j] == num) {
+                count++;
+            }
+        }
+        if (count == 1) return num;
+    }
+    return -1;
+};
