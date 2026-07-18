27381dbbec6b326c397fe49f9e9dac8a4888b6f1
Raj Gupta <168336609+RajGuptaVips2025@users.noreply.github.com>
2026-07-18 14:29:06 +0530

    Time: 11 ms (10.43%), Space: 58 MB (70.14%) - LeetHub

--- 0169-majority-element/0169-majority-element.ts
+++ 0169-majority-element/0169-majority-element.ts
@@ -1,12 +1,29 @@
 function majorityElement(nums: number[]): number {
+    // Better approach
+    const mpp = new Map<number, number>();
+
     for (let i = 0; i < nums.length; i++) {
-        let count = 0;
-        for (let j = 0; j < nums.length; j++) {
-            if (nums[j] == nums[i]) {
-                count++;
-            }
-        }
-        if(count > nums.length/2) return nums[i];
+        mpp.set(nums[i], (mpp.get(nums[i]) || 0) + 1);
     }
+
+    for(const [key, value] of mpp){
+        if(value > Math.floor(nums.length/2)){
+            return key;
+        }
+    }
+
     return -1;
 };
+
+
+// brute force approach
+// for (let i = 0; i < nums.length; i++) {
+//     let count = 0;
+//     for (let j = 0; j < nums.length; j++) {
+//         if (nums[j] == nums[i]) {
+//             count++;
+//         }
+//     }
+//     if(count > nums.length/2) return nums[i];
+// }
+// return -1;
