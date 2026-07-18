da1f88d3c3730919a1f879723dfc68a7f9cb2a24
Raj Gupta <168336609+RajGuptaVips2025@users.noreply.github.com>
2026-07-18 14:09:24 +0530

    Time: 1618 ms (5.06%), Space: 55.8 MB (99.63%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.ts
@@ -0,0 +1,12 @@
+function majorityElement(nums: number[]): number {
+    for (let i = 0; i < nums.length; i++) {
+        let count = 0;
+        for (let j = 0; j < nums.length; j++) {
+            if (nums[j] == nums[i]) {
+                count++;
+            }
+        }
+        if(count > nums.length/2) return nums[i];
+    }
+    return -1;
+};
