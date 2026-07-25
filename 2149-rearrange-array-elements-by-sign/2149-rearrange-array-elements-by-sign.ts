febff7de8bbae5ecb07a8188c29bf0a5094f19ec
Raj Gupta <168336609+RajGuptaVips2025@users.noreply.github.com>
2026-07-25 16:32:24 +0530

    Time: 14 ms (73.56%), Space: 101.5 MB (18.39%) - LeetHub

--- 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.ts
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.ts
@@ -1,16 +1,35 @@
 function rearrangeArray(nums: number[]): number[] {
-    const pos: number[] = [];
-    const neg: number[] = [];
+    // optimal approach
+    let posIndex = 0;
+    let negIndex = 1;
+    const result: number[] = new Array(nums.length);
 
     for (let i = 0; i < nums.length; i++) {
-        if (nums[i] > 0) pos.push(nums[i]);
-        else neg.push(nums[i]);
+        if (nums[i] < 0) {
+            result[negIndex] = nums[i];
+            negIndex += 2;
+        }
+        else {
+            result[posIndex] = nums[i]
+            posIndex += 2;
+        }
     }
-
-    for (let i = 0; i < (nums.length) / 2; i++) {
-        nums[2 * i] = pos[i];
-        nums[2 * i + 1] = neg[i]
-    }
-
-    return nums;
+    return result;
 };
+
+
+// brute force approach
+// const pos: number[] = [];
+// const neg: number[] = [];
+
+// for (let i = 0; i < nums.length; i++) {
+//     if (nums[i] > 0) pos.push(nums[i]);
+//     else neg.push(nums[i]);
+// }
+
+// for (let i = 0; i < (nums.length) / 2; i++) {
+//     nums[2 * i] = pos[i];
+//     nums[2 * i + 1] = neg[i]
+// }
+
+// return nums;
