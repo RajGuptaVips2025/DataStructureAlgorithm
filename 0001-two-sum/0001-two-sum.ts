6ad77d3b3d2797dc4be0ecbde3589d551108a0e8
Raj Gupta <168336609+RajGuptaVips2025@users.noreply.github.com>
2026-07-26 23:24:50 +0530

    Time: 3 ms (62.95%), Space: 56.7 MB (61%) - LeetHub

--- 0001-two-sum/0001-two-sum.ts
+++ 0001-two-sum/0001-two-sum.ts
@@ -1,30 +1,18 @@
 function twoSum(nums: number[], target: number): number[] {
-    // Create a map to store: number -> its index
-    const map = new Map<number, number>();
-    for (let i = 0; i < nums.length; i++) {
-        const complement = target - nums[i];
+    let mpp = new Map<number, number>();
+    let n = nums.length;
+
+    for (let i = 0; i < n; i++) {
+        let complement = target - nums[i];
 
         // Check if the complement already exists in our map
-        if (map.has(complement)) {
-            return [map.get(complement)!, i];
+        if (mpp.has(complement)) {
+            return [mpp.get(complement)!, i];
         }
 
         // If not, store the current number and its index in the map
-        map.set(nums[i], i);
+        mpp.set(nums[i], i);
     }
-
     return [];
+
 };
-
-
-// brute force solution
-// const n = nums.length;
-
-// for (let i = 0; i < n; i++) {
-//     for(let j = i+1; j<n; j++){
-//         if(nums[i] + nums[j] === target){
-//             return [i, j];
-//         }
-//     }
-// }
-// return []
