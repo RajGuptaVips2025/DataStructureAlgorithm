3de1a543d8afdf3b292ce10c7bbd1fd000ba066c
Raj Gupta <168336609+RajGuptaVips2025@users.noreply.github.com>
2026-07-26 22:32:51 +0530

    Time: 0 ms (100%), Space: 53.9 MB (95.2%) - LeetHub

--- 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.ts
+++ 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.ts
@@ -1,9 +1,11 @@
 function check(nums: number[]): boolean {
     let count = 0;
 
-    for(let i = 0; i < nums.length; i++){
-        if(nums[i] > nums[(i+1) % nums.length]) count++;
-        if(count > 1) return false;
+    for (let i = 0; i <= nums.length; i++) {
+        if (nums[i] > nums[(i + 1) % (nums.length)]) {
+            count++;
+        }
+        if (count > 1) return false
     }
     return true;
 };
