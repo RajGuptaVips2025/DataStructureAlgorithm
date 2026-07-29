f8b4bbf4e58e00b803e01aea649a82b6741a0a46
Raj Gupta <168336609+RajGuptaVips2025@users.noreply.github.com>
2026-07-29 16:39:40 +0530

    Time: 0 ms (100%), Space: 58.1 MB (27.15%) - LeetHub

---
+++ 0031-next-permutation/0031-next-permutation.ts
@@ -0,0 +1,34 @@
+/**
+ Do not return anything, modify nums in-place instead.
+ */
+function nextPermutation(nums: number[]): void {
+    let idx = -1;
+    let n = nums.length;
+
+    for (let i = n - 2; i >= 0; i--) {
+        if (nums[i] < nums[i + 1]) {
+            idx = i;
+            break;
+        }
+    }
+
+    if (idx === -1) {
+        nums.reverse();
+        return;
+    }
+
+    for (let i = n - 1; i > idx; i--) {
+        if (nums[i] > nums[idx]) {
+            [nums[i], nums[idx]] = [nums[idx], nums[i]];
+            break;
+        }
+    }
+
+    let left = idx + 1;
+    let right = n - 1;
+    while (left < right) {
+        [nums[left], nums[right]] = [nums[right], nums[left]];
+        left++;
+        right--;
+    }
+};
